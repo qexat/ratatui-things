4a6a319192a3c4750647feff19fb6c1d343331dc
Qexat <43090614+qexat@users.noreply.github.com>
2024-11-18 20:34:28 +0100

    Replace ratatui-hello with the counter app

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,104 @@
 use std::io;
 
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    terminal,
+};
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
+    buffer::Buffer,
+    layout::Rect,
     style::Stylize,
-    widgets::Paragraph,
-    DefaultTerminal,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{block::title, Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
 };
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'esc' or 'q' to quit)")
-                .white()
-                .on_black();
-            frame.render_widget(greeting, frame.area());
-        })?;
+#[derive(Debug, Default)]
+pub struct App {
+    exit: bool,
+    counter: u8,
+}
 
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press {
-                match key.code {
-                    KeyCode::Char('q') | KeyCode::Char('Q') | KeyCode::Esc => return Ok(()),
-                    _ => continue,
-                }
-            }
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
         }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left | KeyCode::Char('-') => self.decrement_counter(1),
+            KeyCode::Right | KeyCode::Char('+') => self.increment_counter(1),
+            _ => {}
+        }
+    }
+
+    fn decrement_counter(&mut self, n: u8) {
+        self.counter = u8::saturating_sub(self.counter, n);
+    }
+
+    fn increment_counter(&mut self, n: u8) {
+        self.counter = u8::saturating_add(self.counter, n);
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buffer: &mut Buffer) {
+        let title = Line::from(" Counter App Tutorial ".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".light_blue().into(),
+            " · ".dim().into(),
+            " Increment ".into(),
+            "<Right>".light_blue().into(),
+            " · ".dim().into(),
+            " Quit ".into(),
+            "<Q>".blue().into(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buffer);
     }
 }
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
-    terminal.clear()?;
+    let app_result = App::default().run(&mut terminal);
 
-    let app_result = run(terminal);
     ratatui::restore();
 
     app_result
