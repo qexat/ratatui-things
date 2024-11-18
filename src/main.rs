de849ff9ce6438858078af20140f63e0cdef5429
Qexat <43090614+qexat@users.noreply.github.com>
2024-11-18 20:06:10 +0100

    Add more keys that exit

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,18 @@ use ratatui::{
 fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
     loop {
         terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'esc' to quit)")
+            let greeting = Paragraph::new("Hello Ratatui! (press 'esc' or 'q' to quit)")
                 .white()
-                .on_blue();
+                .on_black();
             frame.render_widget(greeting, frame.area());
         })?;
 
         if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Esc {
-                return Ok(());
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') | KeyCode::Char('Q') | KeyCode::Esc => return Ok(()),
+                    _ => continue,
+                }
             }
         }
     }
